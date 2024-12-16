3cf97e514eec41f1591cf081b6318c6ca76d9b3a
muhammadbinasim471 <muhammadbinasim471@gmail.com>
2024-12-16 13:36:15 +0000

    NUH UH

--- run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-/home/ubuntu/.nvm/versions/node/v14.5.0/bin/node /home/ubuntu/portfolio/server.js
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo cp ./portfolio.service /etc/systemd/system/
-sudo systemctl enable portfolio.service
-sudo systemctl start portfolio.service
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
